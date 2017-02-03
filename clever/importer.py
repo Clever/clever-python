d6d8f17380d79b75ecd1ab1cf209916f1df3a780
Amelia Jones <amelia.jones@clever.com>
2017-02-03 02:57:17 +0000

    Replace support@ references with tech-support@

--- clever/importer.py
+++ clever/importer.py
@@ -16,6 +16,6 @@ def import_json():
     return simplejson
   except ImportError:
     if _json_loaded is None:
-      raise ImportError("Clever requires a JSON library, which you do not appear to have.  Please install the simplejson library.  HINT: Try installing the python simplejson library via 'pip install simplejson' or 'easy_install simplejson', or contact support@clever.com with questions.")
+      raise ImportError("Clever requires a JSON library, which you do not appear to have.  Please install the simplejson library.  HINT: Try installing the python simplejson library via 'pip install simplejson' or 'easy_install simplejson', or contact tech-support@clever.com with questions.")
     else:
-      raise ImportError("Clever requires a JSON library with the same interface as the Python 2.6 'json' library.  You appear to have a 'json' library with a different interface.  Please install the simplejson library.  HINT: Try installing the python simplejson library via 'pip install simplejson' or 'easy_install simplejson', or contact support@clever.com with questions.")
+      raise ImportError("Clever requires a JSON library with the same interface as the Python 2.6 'json' library.  You appear to have a 'json' library with a different interface.  Please install the simplejson library.  HINT: Try installing the python simplejson library via 'pip install simplejson' or 'easy_install simplejson', or contact tech-support@clever.com with questions.")
